6e2eb42568dfcad54efccb5e22e5d54be13ae5f0
Cheik <cheik.kone@hetic.net>
2019-12-21 20:17:09 +0100

    auto

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let Path = require('path')
 let fs = require('fs')
+const { spawn, spawnSync, exec } = require('child_process');
+const {onExit} = require('@rauschma/stringio');
+let child_process = require('child_process');
+let config = require('./config')
 
+exec('cd | ls', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`exec error: ${err}`);
+    return;
+  }
 
-console.log(autoGit)
+  console.log(`Number of files ${stdout}`);
+});
